d5c7daeb1256a086773bb41c6d93c961faf677e8
MANALI CHOVATIYA <manalichovatiya07@gmail.com>
2023-07-25 11:59:47 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 // server using http
 let http = require('http');
 http.createServer(function(req,res){
-    res.write("Server done!!")
+    res.write("Server done !!")
     res.end();
-}).listen(1200);
+}).listen(8080);
 
 // server uning express
 // const express = require("express")
 // const app = express()
 // app.listen(8080);
+// app.get('/', (req, res)=>{
+//     res.status(200);
+//     res.send("Server is Successfully Running .");
+// });
 
