ffe4d306391c5b0b779182f9d840765d3a7a6754
MANALI CHOVATIYA <manalichovatiya07@gmail.com>
2023-08-09 15:23:54 +0530

    index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-// server using http
 const http = require('http');
-http.createServer(function(req,res){
-    res.write("Server done !!")
-    res.end();
-}).listen(8080);
+const express = require("express");
+const { connectDB } = require("./db/dbconnection");
+const config = require("./config/config");
+
+
+/** Database connection */
+connectDB()
+
+// server using http
+// http.createServer(function(req,res){
+//     res.write("Server done !!")
+//     res.end();
+// }).listen(8080);
 
 // server uning express
-// const express = require("express");
 // const app = express()
 // app.listen(8080);
 // app.get('/', (req, res)=>{
