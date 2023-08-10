52a2532ca8f990ef662fc2d65b705e71cbea46eb
MANALI CHOVATIYA <manalichovatiya07@gmail.com>
2023-08-10 18:16:20 +0530

    routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ const http = require('http');
 const express = require("express");
 const { connectDB } = require("./db/dbconnection");
 const config = require("./config/config");
+const routes = require("./routes/v1");
+const app = express();
 
+/**routes with namespace */
+app.use("/v1", routes);
+
+/** whenever route not created and you try to use that route then throw error. */
+app.use((req, res, next) => {
+  next(new Error("Route not found!"));
+});
 
 /** Database connection */
 connectDB()
@@ -14,8 +23,6 @@ connectDB()
 // }).listen(8080);
 
 // server uning express
-// const app = express()
-// app.listen(8080);
 // app.get('/', (req, res)=>{
 //     res.status(200);
 //     res.send("Server is Successfully Running .");
