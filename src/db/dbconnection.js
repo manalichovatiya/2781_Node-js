ee8b048e2ecc5ad95c15c60388ab219130d42ddc
MANALI CHOVATIYA <manalichovatiya07@gmail.com>
2023-08-20 16:55:38 +0530

    Book-list

--- src/db/dbconnection.js
+++ src/db/dbconnection.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose");
 const config = require("../config/config");
 
 const connectDB = async () => {
-    mongoose
-    .connect(config.mongoose.url , config.mongoose.options)
-    .then((data)=>{console.log("DB connection successfuly!")})
-    .catch((error)=>{console.log("Error:",error)})
+  mongoose
+    .connect(config.mongodb.url, config.mongodb.options)
+    .then((data) => {
+      console.log("Database connection successfully!");
+    })
+    .catch((error) => {
+      console.log("Database connetion error: ", error);
+    });
 };
 
 module.exports = { connectDB };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-const http = require('http');
 const express = require("express");
+const bodyParser = require("body-parser");
+const http = require("http");
 const { connectDB } = require("./db/dbconnection");
+const routes = require("./routes/inedx");
 const config = require("./config/config");
-const routes = require("./routes/v1");
-const app = express();
 
-/**routes with namespace */
+const app = express();
+/**
+ * allow form-data from body
+ * form-data is use for image upload
+ * parse application/x-www-form-urlencoded
+ */
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/**
+ * allow json data from body
+ * parse application/json
+ */
+app.use(bodyParser.json());
+
+/** upload image */
+// app.use(express.static(`./public`));
+
 app.use("/v1", routes);
 
 /** whenever route not created and you try to use that route then throw error. */
@@ -14,16 +30,11 @@ app.use((req, res, next) => {
 });
 
 /** Database connection */
-connectDB()
+connectDB();
 
-// server using http
-// http.createServer(function(req,res){
-//     res.write("Server done !!")
-//     res.end();
-// }).listen(8080);
+/** create server using http */
+const server = http.createServer(app);
 
-// server uning express
-// app.get('/', (req, res)=>{
-//     res.status(200);
-//     res.send("Server is Successfully Running .");
-// });
+server.listen(config.port, () => {
+  console.log("server listning port number "+ config.port);
+});
